ddb9d505102b4b4dc5824f561110be2f5ab83c08
Lyss Garcia <alyssa.g200@gmail.com>
2022-02-12 17:30:46 -0800

    added linked icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import 'materialize-css/dist/css/materialize.min.css';
 
+const styles = {
+    logoStyles: {
+        height: "50px",
+        width: "50px"
+    }
+}
+
 function Footer() {
     return (
         <footer class="page-footer white">
         <div class="footer-copyright">
             <div class="container">
                 <p class="black-text">© 2022 LGG </p>
-                <ul>
-                    <li>
-                        <a class="black-text" href="mailto:alyssa.g200@gmail.com">Contact</a></li>
-                    <li>
-                        <a class="black-text" href="https://github.com/lyssg2">GitHub</a></li>
-                    <li>
-                        <a class="black-text" href="https://www.linkedin.com/in/lyss-garcia-6a480a155/">LinkedIn</a>
-                    </li>
-                </ul>
+                <div class="row center">
+                    <a href="https://github.com/lyssg2"><img style={styles.logoStyles} src={require("../assets/images/github.png")}></img></a>
+                    <a href="https://www.linkedin.com/in/lyss-garcia-6a480a155/"><img style={styles.logoStyles} src={require("../assets/images/linkedin.png")}></img></a>
+                </div>           
             </div>
         </div>
     </footer>
